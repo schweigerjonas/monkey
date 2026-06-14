88e56910561fc73b1433ffd933918282a7ff56db
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-06-14 11:12:41 +0200

    feat: add repl for lexing input

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/schweigerjonas/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the monkey programming language!\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
---
+++ repl/repl.go
@@ -0,0 +1,35 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/schweigerjonas/monkey/lexer"
+	"github.com/schweigerjonas/monkey/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		if scanner.Err() != nil {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
