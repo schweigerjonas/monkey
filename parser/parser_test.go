cf552b315cc9bd3a82d9a099a969988eeb6025f1
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-06-16 12:28:20 +0200

    feat(parser): implement parsing let statements with errors

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -8,15 +8,16 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-		let x = 5;
+		let x 5;
 		let y = 10;
-		let foobar = 171717;
+		let 171717;
 	`
 
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -67,4 +68,20 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
